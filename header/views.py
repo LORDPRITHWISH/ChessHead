4f40c662d58abadc551a35ab6542500129e8548c
LORD PRITHWISH <prithwishchakraborty05@gmail.com>
2024-05-13 03:19:56 +0530

    well progress

--- header/views.py
+++ header/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from . import driver
 
+def index(request):
+    return JsonResponse({'foo': 'bar'})
+
+def player(request):
+    return JsonResponse(driver.allmoves())
 # Create your views here.
